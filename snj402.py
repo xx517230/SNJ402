dd29c3fd087fe16064853368b295576168e16d3c
Ki J <xx517230@gmail.com>
2024-08-26 17:43:16 +0800

    20240826

--- snj402.py
+++ snj402.py
@@ -1,27 +1,103 @@
 import os, re
 
-# 客供pattern解析所需的正则
-re_fileFormatCommentCheck = re.compile(r"^\/\/")  # 开头两行注释
+"""
+SNJ402 vcd文件转换思路
 
+1. 校验vcd文件
+    校验vcd文件每行都满足我们的正则规则，保证了SNJ402 vcd文件完全符合我们的预期，不出现错漏。若匹配有问题，需完善正则规则。
+    转换pattern时必须检验，之后的程序就不需要再判断格式是否满足要求。
+2. 转换vcd文件
+
+    1. 获取管脚名同时实现可用于输出pattern文件中的管脚列表，形式同vcd的管脚形式，方便查看
+    2. 获取0时刻初始状态
+    3. 遍历每行，获取时刻点，该时刻点减去上一行的时刻点就是上一行pattern状态的维持时间。
+    4. 将每行的pattern状态的时间统计timerDict（时间=>次数），若不满足要求，则需特殊处理，一般情况下是能满足的
+    5. 重新遍历vcd文件，获取每行patten的状态及时间，根据timerDict来判断是否需要修改TS，生成未压缩的简单pattern（该pattern需要确认是否转换正常，压缩的pattern不好确认）
+    6. 对未压缩的pattern压缩，使用RPT指令（最好增加通过解压RPT指令来生成未压缩的pattern，用于核对转换是否正常）
+"""
+
+# VCD文件sample
+"""
+//Power on, After power on(< 1ms), PA[3]/PA[2] set i2c command to enter I2C mode,
+//wait 100us, PB[3]/PB[2] send Command 
+<< time      | ADR >> PP PP // SITE
+<< ..........| ....>> AA BB // ..........
+<< ..........| ....>> 32 32 // ..........
+<< ..........| ....>> .. .. // ..........
+<< ..........| ....>> .. .. // ..........
+<< ..........| ....>> .. .. // ..........
+<< ..........| ....>> .. .. // ..........
+<< ..........| ....>> ii ib // ..........
+<< ..........| ....>> 11 XX // wait about 1060 clock enter SLEEP
+<<        438| xx00>> 00 00 // 00000003
+<<        563| xx00>> 00 00 // 00000004
+<<        688| xx00>> 00 00 // 00000005
+<<        813| xx00>> 10 00 // 00000006
+<<        938| ffff>> 10 00 // 00000007
+"""
+
+
+# 客供pattern核对所需的正则
+# << time      | ADR >> PP PP // SITE
+re_fileFormatCommentCheck = re.compile(r"^\/\/")  # 开头两行注释
+# << time      | ADR >> PP PP // SITE
+re_fileFormatItemCheck = re.compile(
+    r"^<<\s+time\s+\|\s+ADR\s+>>\s+[a-zA-Z ]+\s+//\sSITE$"
+)
+# << ..........| ....>> AA BB // ..........
+re_fileFormatItemPinBodyCheck = re.compile(
+    r"^<<\s+\.+\|\s+\.+>>\s+[a-zA-Z0-9_ ]+//\s+\.+$"
+)
+# << ..........| ....>> .. .. // ..........
+re_fileFormatItemBlankCheck = re.compile(r"^<<\s+\.+\|\s+\.+>>[ .]+//\s+\.+$")
+
+"""
+<< ..........| ....>> ii ib // ..........
+<< ..........| ....>> 11 XX // wait about 1060 clock enter SLEEP
+"""
+re_fileFormatItemEndCheck = re.compile(
+    r"^<<\s+\.+\|\s+\.+>>\s+[a-zA-Z0-9 ]+//\s+[a-zA-Z0-9 ]+$"
+)  # ... ... ... end
+
+# <<        438| xx00>> 00 00 // 00000003
+re_fileFormatBodyCheck = re.compile(r"^<<\s+\d+\|\s+\w+>>\s+[01HXLZz ]+\s+//\s+.*?$")
+
+re_fileFormCheckRegexPlainStrDict = {
+    r"^\/\/"=False,
+    r"^<<\s+time\s+\|\s+ADR\s+>>\s+[a-zA-Z ]+\s+//\sSITE$"=False,
+    r"^<<\s+\.+\|\s+\.+>>\s+[a-zA-Z0-9_ ]+//\s+\.+$"=False,
+    r"^<<\s+\.+\|\s+\.+>>[ .]+//\s+\.+$"=False,
+    r"^<<\s+\d+\|\s+\w+>>\s+[01HXLZz ]+\s+//\s+.*?$"=False,
+    r"^<<\s+\d+\|\s+\w+>>\s+[01HXLZz ]+\s+//\s+.*?$",
+}
+
+'''
+re_fileFormatCommentCheck = re.compile(r"^\/\/")  # 开头两行注释
+# << time      | ADR >> PP PP // SITE
 re_fileFormatItemCheck = re.compile(
     r"^<<\s+time\s+\|\s+ADR\s+>>\s+(?P<pinHead>[a-zA-Z ]+)\s+//\sSITE$"
-)  # time/adr/pin/line
-
+)
+# << ..........| ....>> AA BB // ..........
 re_fileFormatItemPinBodyCheck = re.compile(
-    r"^<<\s+\.+\|\s+\.+>>\s+(?P<pinBody>[a-zA-Z0-9 ]+)//\s+\.+$"
-)  # ...pinBody...
+    r"^<<\s+\.+\|\s+\.+>>\s+(?P<pinBody>[a-zA-Z0-9_ ]+)//\s+\.+$"
+)
+# << ..........| ....>> .. .. // ..........
+re_fileFormatItemBlankCheck = re.compile(r"^<<\s+\.+\|\s+\.+>>[ .]+//\s+\.+$")
 
-re_fileFormatItemBlankCheck = re.compile(
-    r"^<<\s+\.+\|\s+\.+>>[ .]+//\s+\.+$"
-)  # ... ... ...
-
+"""
+<< ..........| ....>> ii ib // ..........
+<< ..........| ....>> 11 XX // wait about 1060 clock enter SLEEP
+"""
 re_fileFormatItemEndCheck = re.compile(
     r"^<<\s+\.+\|\s+\.+>>\s+(?P<pattern>[a-zA-Z0-9 ]+)//\s+(?P<comment>[a-zA-Z0-9 ]+)$"
 )  # ... ... ... end
 
+# <<        438| xx00>> 00 00 // 00000003
 re_fileFormatBodyCheck = re.compile(
     r"^<<\s+(?P<time>\d+)\|\s+\w+>>\s+(?P<pattern>[01HXLZz ]+)\s+//\s+(?P<comment>.*)?$"
 )
+'''
+
 
 # 获取当前目录
 workPath = os.getcwd()
