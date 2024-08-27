75cb1d87eeb195d843921ffc3e8b43977cf58108
Ki J <xx517230@gmail.com>
2024-08-27 17:44:11 +0800

    20240827

--- snj402.py
+++ snj402.py
@@ -1,4 +1,4 @@
-import os, re
+import os, re, time
 
 """
 SNJ402 vcd文件转换思路
@@ -35,6 +35,8 @@ SNJ402 vcd文件转换思路
 <<        813| xx00>> 10 00 // 00000006
 <<        938| ffff>> 10 00 // 00000007
 """
+startTime = time.time()
+checkFlag = False
 
 # 客供pattern核对所需的正则
 # << time      | ADR >> PP PP // SITE
@@ -62,9 +64,9 @@ re_fileFormatItemEndCheck = re.compile(
 re_fileFormatBodyCheck = re.compile(r"^<<\s+\d+\|\s+\w+>>\s+[01HXLZz ]+\s+//\s+.*?$")
 
 
-
 # 获取当前目录
 workPath = os.getcwd()
+# sourceFile = r"TPn_I2C_program_220302.txt"
 sourceFile = r"TPn_I2C_Read_IDx.txt"
 outputFile = r"I2C_Read_IDx.pat"
 # sourceFile = r"I2C_program.txt"
@@ -73,99 +75,46 @@ outputFile = r"I2C_Read_IDx.pat"
 # outputFile = r"I2C_trim_32K.pat"
 
 # 校验vcd
+fileFormatCheckRegexDict = {
+    re_fileFormatCommentCheck: 0,
+    re_fileFormatItemCheck: 0,
+    re_fileFormatItemPinBodyCheck: 0,
+    re_fileFormatItemBlankCheck: 0,
+    re_fileFormatItemEndCheck: 0,
+    re_fileFormatBodyCheck: 0,
+}
+if checkFlag:
+    SOURCEFILE = os.path.join(workPath, sourceFile)
 
-with open(os.path.join(workPath, sourceFile), "r") as fp:
-    for lineCnt, line in enumerate(fp, start=1):
-        for checkDict in fileFormatCheckDict.values():
+    with open(SOURCEFILE, "r") as fp:
+        for lineCnt, line in enumerate(fp, start=1):
+            if re_fileFormatBodyCheck.search(line):
+                fileFormatCheckRegexDict[re_fileFormatBodyCheck] += 1
+                continue
+            if re_fileFormatCommentCheck.search(line):
+                fileFormatCheckRegexDict[re_fileFormatCommentCheck] += 1
+                continue
+            if re_fileFormatItemCheck.search(line):
+                fileFormatCheckRegexDict[re_fileFormatItemCheck] += 1
+                continue
+            if re_fileFormatItemPinBodyCheck.search(line):
+                fileFormatCheckRegexDict[re_fileFormatItemPinBodyCheck] += 1
+                continue
+            if re_fileFormatItemBlankCheck.search(line):
+                fileFormatCheckRegexDict[re_fileFormatItemBlankCheck] += 1
+                continue
+            if re_fileFormatItemEndCheck.search(line):
+                fileFormatCheckRegexDict[re_fileFormatItemEndCheck] += 1
+                continue
 
-        pass
+    for regex, count in fileFormatCheckRegexDict.items():
+        print(f"{regex} = {count}")
+
 
 SOURCEFILE = open(os.path.join(workPath, sourceFile), "r")
 OUTPUTFILE = open(os.path.join(workPath, outputFile), "w")
 
-lineCnt = 0
-pinEndFlag = 0
-patternStartFlag = 0
-oldTimeStr = "0"  # 用于保存上一行的时刻点
-timeStr = "0"  # 用于保存当前行的时刻点
-time = 0  # 用于保存状态维持时间
-pattern = ""  # 用于获取pattern内容
-comment = ""  # 用于保存pattern文件中的注释行数及指令说明
-pinStr = []  # 用于保存pattern文件中的管脚
-timeSet = (
-    set()
-)  # 用于保存本pattern中所有时间的集合，用于TS的确认，因为按本pattern的时间，测试机精度无法达到
-
-timeDict = {}  # 用于保存"时间:时间出现次数"
-while True:
-    fileLine = SOURCEFILE.readline()
-    lineCnt += 1
-    if not fileLine:  # 文件末尾跳出
-        break
-    if patternStartFlag == 0:
-        if re_fileFormatCommentCheck.search(fileLine):
-            if lineCnt < 3:
-                continue
-            else:
-                print("pattern %d行存在注释,请确认格式!!!" % lineCnt)
-        if re_fileFormatItemCheck.search(fileLine):
-            str = re_fileFormatItemCheck.search(fileLine).group("pinHead")
-            pinStr.append(str)
-        if re_fileFormatItemPinBodyCheck.search(fileLine) and pinEndFlag == 0:
-            str = re_fileFormatItemPinBodyCheck.search(fileLine).group("pinBody")
-            pinStr.append(str)
-        if re_fileFormatItemBlankCheck.search(fileLine):
-            pinEndFlag = 1
-        if re_fileFormatItemEndCheck.search(fileLine) and pinEndFlag == 1:
-            patternStartFlag = 1
-            pattern = re_fileFormatItemEndCheck.search(fileLine).group("pattern")
-            comment = re_fileFormatItemEndCheck.search(fileLine).group("comment")
-            for i in pinStr:
-                OUTPUTFILE.write("//")
-                OUTPUTFILE.write(i)
-                OUTPUTFILE.write("\n")
-        continue
-    if re_fileFormatBodyCheck.search(fileLine):
-        oldTimeStr = timeStr
-        timeStr = re_fileFormatBodyCheck.search(fileLine).group("time")
-        time = int(timeStr) - int(oldTimeStr)  # 本行的时间
-        timeSet.add(time)
-        if time > 1:
-            OUTPUTFILE.write(" *%s*RPT %d; // %s" % (pattern, time, comment))
-            OUTPUTFILE.write("\n")
-        else:
-            OUTPUTFILE.write(" *%s*; // %s" % (pattern, comment))
-            OUTPUTFILE.write("\n")
-        pattern = re_fileFormatBodyCheck.search(fileLine).group("pattern")
-        comment = re_fileFormatBodyCheck.search(fileLine).group("comment")
-    else:
-        print("check body line failed to match pattern!!!")
-        print("line=%d, %s not match!!!" % (lineCnt, fileLine))
-        break
-print(timeSet)
-SOURCEFILE.close()
-OUTPUTFILE.close()
 
 
-for time in timeSet:
-    timeDict[time] = 0
-
-SOURCEFILE = open(os.path.join(workPath, sourceFile), "r")
-lineCnt = 0
-oldTimeStr = "0"  # 用于保存上一行的时刻点
-timeStr = "0"  # 用于保存当前行的时刻点
-time = 0  # 用于保存状态维持时间
-while True:
-    fileLine = SOURCEFILE.readline()
-    lineCnt += 1
-    if not fileLine:  # 文件末尾跳出
-        break
-    if re_fileFormatBodyCheck.search(fileLine):
-        oldTimeStr = timeStr
-        timeStr = re_fileFormatBodyCheck.search(fileLine).group("time")
-        time = int(timeStr) - int(oldTimeStr)  # 本行的时间
-        timeDict[time] += 1
-
-for key in timeDict.keys():
-    print("%d=>%d" % (key, timeDict[key]))
-SOURCEFILE.close()
+endTime = time.time()
+print(f"时间总共花费: {round(endTime-startTime, 3)} S")
